ca34bd6a022e86adeeb3b5308a9fbf1ebb5b9f5f
Adam Wolnikowski <awolnikowski@gmail.com>
2020-05-20 21:34:08 -0400

    added zc706 cmake rules

--- cmake/modules/VTA.cmake
+++ cmake/modules/VTA.cmake
@@ -93,6 +93,7 @@ elseif(PYTHON)
     file(GLOB FPGA_RUNTIME_SRCS vta/runtime/*.cc)
     # Rules for Zynq-class FPGAs with pynq OS support (see pynq.io)
     if(${VTA_TARGET} STREQUAL "pynq" OR
+       ${VTA_TARGET} STREQUAL "zc706" OR
        ${VTA_TARGET} STREQUAL "ultra96")
       list(APPEND FPGA_RUNTIME_SRCS ${VTA_HW_PATH}/src/pynq/pynq_driver.cc)
       # Rules for Pynq v2.4
@@ -108,6 +109,7 @@ elseif(PYTHON)
       target_compile_definitions(vta PUBLIC ${__strip_def})
     endforeach()
     if(${VTA_TARGET} STREQUAL "pynq" OR
+       ${VTA_TARGET} STREQUAL "zc706" OR
        ${VTA_TARGET} STREQUAL "ultra96")
       target_include_directories(vta PUBLIC ${VTA_HW_PATH}/include)
       target_link_libraries(vta ${__cma_lib})
