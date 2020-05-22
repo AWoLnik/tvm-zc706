9779c4b4e935dc684178b83d5cfa19b79ebc82b3
Adam Wolnikowski <awolnikowski@gmail.com>
2020-05-21 20:42:35 -0400

    added zc706 support to rpc server

--- vta/python/vta/exec/rpc_server.py
+++ vta/python/vta/exec/rpc_server.py
@@ -67,7 +67,7 @@ def server_start():
     def program_fpga(file_name):
         # pylint: disable=import-outside-toplevel
         env = get_env()
-        if env.TARGET == "pynq":
+        if env.TARGET == "pynq" or env.TARGET == "zc706":
             from pynq import xlnk
             # Reset xilinx driver
             xlnk.Xlnk().xlnk_reset()
